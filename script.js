0c3b2dee3a88e94d0a29cfc616c67c9947cee017
Guillermo Castano <guillermo.c.velez@gmail.com>
2020-11-04 06:46:10 -0500

    primer commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.modal').modal();
+});
+
+
+/* $(document).ready(function(){
+    $('.sidenav').sidenav();
+});
+ */
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
